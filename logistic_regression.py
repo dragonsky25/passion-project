527d2dcba2b434526141f69cfdddebb22a64f0ee
dragonsky25 <glem.alisa@gmail.com>
2025-02-14 16:37:55 +0300

    hyperparameter optimization

--- logistic_regression.py
+++ logistic_regression.py
@@ -3,23 +3,22 @@ from sklearn.metrics import classification_report
 from sklearn.model_selection import GridSearchCV
 
 def log_regression(x_train, y_train, x_test):
-    logreg = LogisticRegression(random_state=16)
-    logreg = LogisticRegression(random_state=16, max_iter=1000)
-    logreg.fit(x_train, y_train)
     param_grid = [
         {'C': [1, 10, 100, 1000],
-         'max_iter': [100, 1000, 2500, 5000, 10000],
          'class_weight': ['balanced']
          }
-    logreg = LogisticRegression(random_state=16, max_iter=10000)
-    clf = GridSearchCV(logreg, param_grid=param_grid, cv=3, verbose=True, n_jobs=-1)
-    best_clf = clf.fit(x_train, y_train)
-    y_pred = logreg.predict(x_test)
-    print(f'Accuracy - : {best_clf.score(x_train, y_train):.3f}')
-    return y_pred
+    ]
+    logreg_0 = LogisticRegression(random_state=16, max_iter=1000)
+    logreg_cv = GridSearchCV(logreg_0, param_grid=param_grid, cv=3, verbose=True, n_jobs=-1)
+    best_logreg = logreg_cv.fit(x_train, y_train)
+    print(best_logreg.best_params_)
+    print(best_logreg.best_estimator_)
+    y_predict = best_logreg.predict(x_test)
+    return y_predict
 
-def evaluate_model(y_true, y_pred):
-    report = classification_report(y_true, y_pred)
+def evaluate_model(y_true, y_predict):
+    report = classification_report(y_true, y_predict)
     return report
+
 
 
--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from sklearn.metrics import accuracy_score, classification_report
 
 
 def main():
-  #collecting tokens
   #data loading
   filepath = '/Passion project/text.csv'
   data = load_data(filepath)
@@ -20,36 +19,25 @@ def main():
 
   #extracting features
   feature_matrix = extract_features(cleaned_texts_joined)
-  training, testing, = train_test_split(
-  feature_matrix, test_size = 0.2, shuffle = False
-  #splitting matrix into train & test
   labels = data['emotion']
 
   #splitting matrix into train & test
   x_train, x_test, y_train, y_test = train_test_split(
-  feature_matrix, labels ,test_size = 0.2, shuffle = False
       feature_matrix, labels ,test_size = 0.2, shuffle = False
   )
-  return testing
 
   #predicting labels
   y_predict = log_regression(x_train, y_train, x_test)
-  return y_predict
 
   for text, predicted, actual in zip(data['text'][:10], y_predict[:10], y_test[:10]):
     print(f"Text: {text}.\nPredicted emotion: {predicted}.\nActual emotion: {actual}.\n{'-' * 50}")
 
   print(accuracy_score(y_test, y_predict))
   print(classification_report(y_test, y_predict))
-  #print(accuracy_score(y_test, y_predict))
-  #print(classification_report(y_test, y_predict))
 
   return y_predict, y_test
 
 if __name__ == "__main__":
-  print(main())
   y_pred, y_test = main()
   print("Predicted labels:", y_pred)
   print("Actual labels:", y_test)
-  #print("Predicted labels:", y_pred)
-  #print("Actual labels:", y_test)
